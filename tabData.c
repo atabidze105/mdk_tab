d1097375b5b3076b6f63d0401d3969f890368699
atabidze105 <114877662+atabidze105@users.noreply.github.com>
2024-03-13 15:13:57 +0300

    Add files via upload

---
+++ tabData.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int args, char** argv)
+{
+	printf("size of int8_t: %lu\n", sizeof(int8_t)); //char
+	printf("size of int16_t: %lu\n", sizeof(int16_t)); //short
+	printf("size of int32_t: %lu\n", sizeof(int32_t)); //int
+	printf("size of int64_t: %lu\n", sizeof(int64_t)); //long
+
+	printf("size of int_least8_t: %lu\n",sizeof(int_least8_t));//char
+	printf("size of int_least16_t: %lu\n",sizeof(int_least16_t));//short
+	printf("size of int_least32_t: %lu\n",sizeof(int_least32_t));//int
+	printf("size of int_least64_t: %lu\n",sizeof(int_least64_t));//long
+
+	printf("size of int_fast8_t: %lu\n",sizeof(int_fast8_t));//char
+	printf("size of int_fast16_t: %lu\n",sizeof(int_fast16_t));//short
+	printf("size of int_fast32_t: %lu\n",sizeof(int_fast32_t));//int
+	printf("size of int_fast64_t: %lu\n",sizeof(int_fast64_t));//long
+
+	printf("size of intmax_t: %lu\n",sizeof(intmax_t));//long
+							       
+	return 0;
+}
